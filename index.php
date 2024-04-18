6d8aa4ee152141b174965633f7d2dd85320aa847
emzyfrosh12 <ej08123707762@gmail.com>
2024-04-18 11:49:12 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+function greet($name){
+    return 'Good morning '. $name;
+}
+
+// echo greet('James');
+
+$arr = ['Moses Bliss', 'Telema Jons', 'John Cena', 'Jeff Bezos'];
+
+foreach ($arr as $ar){
+    echo 'my name is '. $ar.'<br>';
+
+
+}
+
+
+// foreach($arr as $ar){
+//     echo $ar. '<br>';
+// }
