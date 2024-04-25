04e2e49a0027bf5314c2807acf30215521a732c6
emzyfrosh12 <167190608+emzyfrosh12@users.noreply.github.com>
2024-04-25 11:26:58 +0200

    Add files via upload

---
+++ ludo.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>ROLL THE DICE</h1>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-4">
+<p>
+<img src="diles<?php echo rand('1', '6');?>.png" alt="picture">
+</p>
+
+
+
+</div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
